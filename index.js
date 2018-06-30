200eb3a6b4f25e47061a4586d6278e6802453425
Chris Troutner <chris.troutner@gmail.com>
2018-06-30 12:03:54 -0700

    Working

--- index.js
+++ index.js
@@ -4,4 +4,24 @@
 "use strict"
 
 var noip = require('no-ip');
-var config = config.js;
+var config = require('./config.js').config;
+
+console.log(`config: ${JSON.stringify(config,null,2)}`);
+
+var mynoip = new noip({
+  hostname: config.hostname,
+  user: config.user,
+  pass: config.password
+})
+
+mynoip.on('error', function(err){
+  console.log(err)
+})
+
+mynoip.on('success', function(isChanged, ip){
+  console.log(isChanged, ip)
+})
+
+//noip.update() // Manual update, you can also provide a custom IP address
+
+mynoip.start() // start an automatic renewal every 1h by default or provide a custom ms.
