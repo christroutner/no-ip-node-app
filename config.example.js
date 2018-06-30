5abad774e803a3adf40075078ece8b2ea7ae868a
Chris Troutner <chris.troutner@gmail.com>
2018-06-30 12:05:11 -0700

    Adding a dummy config file

---
+++ config.example.js
@@ -0,0 +1,12 @@
+/*
+  Edit this file with the appropriate no-ip settings.
+  Then rename the file to config.js
+*/
+
+"use strict"
+
+module.exports.config = {
+  hostname: 'your-hostname.hopto.org',
+  user: 'your@email.com',
+  password: 'yourPassword'
+};
